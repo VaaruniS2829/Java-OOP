83b803d271a2679e82cc0032ac064d9dbb8a399a
Vaaruni S <vaarunis20@gmail.com>
2025-08-24 21:18:02 +0530

    ConstructorOverloading.java

---
+++ ConstructorOverloading.java
@@ -0,0 +1,22 @@
+class ConstructorOverloading{
+
+    int a;
+    int b;
+
+    // Creating constructor without giving input
+    ConstructorOverloading(){
+
+        a=10;
+        b=20;
+        System.out.println(a+b);
+
+    }
+     
+    // Creating constructor by giving input
+    ConstructorOverloading(int x , int y){
+
+        int a=x;
+        int b=y;
+        System.out.println(a+b);
+
+    }
