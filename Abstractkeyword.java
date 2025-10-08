2e47f5139f8f1dd658161ad1013108994e026dcf
Vaaruni S <vaarunis20@gmail.com>
2025-10-08 23:23:05 +0530

    Abstractkeyword.java

---
+++ Abstractkeyword.java
@@ -0,0 +1,44 @@
+abstract class Car // abstract class
+
+{
+
+ public abstract void drive(); // abstract method
+
+ public void playMusic()
+
+ {
+
+    System.out.println("play music");
+
+ }
+ 
+}
+ 
+class WagenR extends Car
+
+{
+
+ public void drive()
+
+ {
+
+     System.out.println("Driving..");
+
+ }
+
+}
+
+public class Abstractkeyword
+{
+
+    public static void main(String[] args)
+     {
+        // Car obj = new Car();  Here , we cannot create object of class Car
+        Car obj = new WagenR();
+
+        obj.playMusic();
+
+        obj.drive();
+
+    }
+}
