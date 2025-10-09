c6e761b83170417018aa7fc5585d47d7d77c73d5
Vaaruni S <vaarunis20@gmail.com>
2025-10-09 23:54:31 +0530

    MethodOverriding.java

--- MethodOverriding.java
+++ MethodOverriding.java
@@ -1,4 +1,6 @@
-// Method Overriding: 
+// Method Overriding: Method overriding occurs when a subclass defines a method with the
+// same name , return type , and parameters as a method in its superclass , but provides
+// a different implementation.
 class P
      {
     public void show()
