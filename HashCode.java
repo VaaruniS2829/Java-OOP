ffd8d2d9f1ae585de1cd30422c8e0cc4ef238309
Vaaruni S <vaarunis20@gmail.com>
2025-10-12 11:47:03 +0530

    HashCode.java

---
+++ HashCode.java
@@ -0,0 +1,11 @@
+class HashCode {
+    public static void main(String[] args) {
+        String name1 = "Anjali";
+        String name2 = "Kavya";
+        String name3 = "Sushanth";
+
+        System.out.println("hashCode of name1: " + name1.hashCode());
+        System.out.println("hashCode of name2: " + name2.hashCode());
+        System.out.println("hashCode of name3: " + name3.hashCode());
+    }
+}
