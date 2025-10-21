a9baa364729c87739fa092521d530bc2fbcaa187
Vaaruni S <vaarunis20@gmail.com>
2025-10-21 22:02:43 +0530

    EnumClass.java

---
+++ EnumClass.java
@@ -0,0 +1,37 @@
+// enum is a class , inside it we can create our own variables and constructors
+enum Laptop 
+{
+   Macbook(2000) , XPS(2200) , Surface , ThinkPad(1800);// in dollars
+   private int price;
+   private Laptop()
+   {
+      price=500;
+   }
+   private Laptop(int price)
+   {
+      this.price=price;
+      System.out.println("in Laptop " + this.name());
+   }
+    
+   public int getPrice()
+   {
+      return price;
+   }
+
+   public void setPrice(int price)
+   {
+      this.price=price;
+   }
+}
+public class EnumClass
+{
+   public static void main(String[] args)
+    {
+      //Laptop lap = Laptop.Macbook;
+      //System.out.println(lap + ":" + lap.getPrice());
+      for (Laptop lap:Laptop.values())
+      {
+         System.out.println(lap + ":" + lap.getPrice());
+      }
+    }
+}
