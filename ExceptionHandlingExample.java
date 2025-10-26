88c22ceafa2ad21c6a89f571e836f5334893a507
Vaaruni S <vaarunis20@gmail.com>
2025-10-26 23:01:14 +0530

    ExceptionHandlingExample.java

---
+++ ExceptionHandlingExample.java
@@ -0,0 +1,34 @@
+package oopjavalabpackage;
+
+
+
+public class ExceptionHandling {
+
+	public static void main(String[] args) {
+
+		try {
+
+			int numerator = 10;
+
+			int denominator = 0;
+
+			int result = numerator / denominator;
+
+		} // This line throw an exception
+
+	catch(ArithmeticException e){
+
+			System.err.println("Error : Cannot divide by zero .");
+
+			// This block execute because of the division by zero
+
+		}
+
+	System.out.println(" Program continues after handling the exception .");
+
+		
+
+	}
+
+}
+
