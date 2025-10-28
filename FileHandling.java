8afa0c7982be034e74add5c553d35975b9ea531c
Vaaruni S <vaarunis20@gmail.com>
2025-10-28 19:43:10 +0530

    FileHandling.java

---
+++ FileHandling.java
@@ -0,0 +1,18 @@
+import java.io.File;
+
+public class FileHandling {
+    public static void main(String[] args)
+    {
+        String path="C:\\Users\\VAARUNI\\Desktop\\data.txt";
+        File file = new File(path);
+       System.out.println(file.exists()); // checks if file exists or not
+       System.out.println(file.canRead());// checks whether we can read or not
+       System.out.println(file.canWrite()); // checks whether we can write or not
+       System.out.println(file.getName()); // it prints the name of file
+       System.out.println(file.getParent()); // it prints the path of parent folder
+       System.out.println(file.getAbsolutePath()); // it is used to get complete path or directory , i.e.., it return only string
+       System.out.println(file.getAbsoluteFile()); // (format is change) returns a file object i.e.., we'll get actuall file
+       System.err.println(file.isFile()); // it checks if it is file or directory
+       System.err.println(file.isDirectory()); // it checks if it is file or directory
+    }
+}
