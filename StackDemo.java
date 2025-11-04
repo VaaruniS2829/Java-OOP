af8ccc82d8bca77fb97103d0615eb55889884308
Vaaruni S <vaarunis20@gmail.com>
2025-11-04 21:57:32 +0530

    StackDemo.java

---
+++ StackDemo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class StackExample {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        stack.push("A");
+        stack.push("B");
+        stack.push("C");
+
+        System.out.println("Stack: " + stack);
+        System.out.println("Top element: " + stack.peek());
+        System.out.println("Popped element: " + stack.pop());
+        System.out.println("Stack after pop: " + stack);
+    }
+}
