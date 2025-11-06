8252b09ed4093ca6c849273f1e8be8f8f7084660
Vaaruni S <vaarunis20@gmail.com>
2025-11-06 23:18:23 +0530

    SortingExample.java

---
+++ SortingExample.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.List;
+class SortingExample1{
+public static void main(String[] args) {
+List<Integer> nums = new ArrayList<>();
+nums.add(2);
+nums.add(1);
+nums.add(5);
+nums.add(8);
+Collections.sort(nums);
+System.out.println(nums);
+}
+}
